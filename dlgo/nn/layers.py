79625ed0b0e55ce9cd594984d9996afa0329bae3
ababazhi <abibazhi@126.com>
2024-02-28 19:55:04 +0800

    手工提交

---
+++ dlgo/nn/layers.py
@@ -0,0 +1,22 @@
+import numpy as np
+# end::imports[]
+
+
+# tag::sigmoid[]
+def sigmoid_double(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+
+def sigmoid(z):
+    return np.vectorize(sigmoid_double)(z)
+# end::sigmoid[]
+
+
+# tag::sigmoid_prime[]
+def sigmoid_prime_double(x):
+    return sigmoid_double(x) * (1 - sigmoid_double(x))
+
+
+def sigmoid_prime(z):
+    return np.vectorize(sigmoid_prime_double)(z)
+# end::sigmoid_prime[]
--- dlgo/nn/layers/sigmoid_double.py
+++
@@ -1,5 +0,0 @@
-def sigmoid_double(x):
-  return 1.0 / (1.0 + np.exp(-x))
-    
-def sigmoid(z):
-  return np.vectorize(sigmoid_double)(z)
