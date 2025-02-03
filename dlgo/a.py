048c03d878fced48a9db36ee1ecf612bffc1f3a3
ababazhi <abibazhi@126.com>
2025-02-03 16:01:12 +0800

    check and commit all

--- dlgo/a.py
+++
@@ -1,6 +0,0 @@
-print(123)
-import sys
-print(sys.path)
-
-def add(x, y):
-    return x + y
---
+++ dlgo/learn.py
@@ -0,0 +1,13 @@
+print(123)
+import sys
+print(sys.path)
+
+from datetime import datetime
+
+def add(x, y):
+    return x + y
+
+def printNow():
+    print(datetime.now())
+
+printNow()
